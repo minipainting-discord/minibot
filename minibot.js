83006d02ba1d211bf0b1a1fa59071ecf62c038db
madx <madx+github@yapok.org>
2019-07-25 09:56:39 +0200

    bot: Simplify the bot run script

--- minibot.js
+++ minibot.js
@@ -1,24 +1,20 @@
 const spawn = require("child_process").spawn
 const exec = require("child_process").exec
-let node_bot = 0
 
-function bot_logging(data) {
-  console.log("bot_data: " + data)
+function log(data) {
+  console.log(String(data).trim())
 }
 
-function bot_restart() {
-  let gitpull = exec("git pull")
-  gitpull.on("close", () => {
-    setTimeout(() => {
-      node_bot = spawn("node", ["index.js"])
-      node_bot.on("close", bot_restart)
-      node_bot.stdout.on("data", bot_logging)
-      node_bot.stderr.on("data", bot_logging)
-    }, 2000)
-  })
+function run() {
+  const bot = spawn("node", ["index.js"])
+  bot.on("close", restart)
+  bot.stdout.on("data", log)
+  bot.stderr.on("data", log)
 }
 
-node_bot = spawn("node", ["index.js"])
-node_bot.on("close", bot_restart)
-node_bot.stdout.on("data", bot_logging)
-node_bot.stderr.on("data", bot_logging)
+function restart() {
+  const gitPull = exec("git pull")
+  gitPull.on("close", () => setTimeout(run, 2000))
+}
+
+run()
