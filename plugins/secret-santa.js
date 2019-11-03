9aee038c746fc5706374f8f24e00076be5165b8e
madx <madx+github@yapok.org>
2019-11-03 12:46:33 +0100

    secret-santa: Add a web view to see letters

--- plugins/secret-santa.js
+++ plugins/secret-santa.js
@@ -1,4 +1,11 @@
+const settings = require("../settings.json")
+
 const SANTA_USAGE = "`!santa list | !santa pair`"
+
+const TIER_THRESHOLD = 3000
+const NICE_TIER = "nice"
+const NAUGHTY_TIER = "nice"
+const WEB_ROUTE = "/admin/santa"
 
 module.exports = {
   name: "secret-santa",
@@ -6,7 +13,7 @@ module.exports = {
   setup: bot => {
     bot.data.set("secret-santa:pending", new Map())
     return bot.db.run(
-      "CREATE TABLE IF NOT EXISTS secretSanta (userId TEXT, letter TEXT, address TEXT, region TEXT)",
+      "CREATE TABLE IF NOT EXISTS secretSanta (userId TEXT PRIMARY KEY, letter TEXT, address TEXT, region TEXT, tier TEXT)",
     )
   },
 
@@ -19,9 +26,24 @@ module.exports = {
     },
   ],
 
-  dmFilter(bot, message) {
+  web: (app, bot) => {
+    app.get(WEB_ROUTE, santaHandler(bot))
+  },
+
+  async dmFilter(bot, message) {
+    const guild = bot.client.guilds.first()
+
     const pendingLetters = bot.data.get("secret-santa:pending")
     if (message.author === bot.client.user) {
+      return
+    }
+
+    const guildMember = await guild.members.find(
+      u => u.id === message.author.id,
+    )
+
+    if (!guildMember) {
+      message.reply("Join us at https://discord.gg/WREsGYF")
       return
     }
 
@@ -48,6 +70,22 @@ module.exports = {
 
       if (!letter.address) {
         letter.address = message.content
+        message.reply(
+          [
+            "Great, now please tell me how many total messages have you posted around here?",
+            "You can find this info by searching for your own messages like so http://drop.yapok.org/2019-11-03T11:42:14.png",
+          ].join("\n"),
+        )
+        return true
+      }
+
+      if (!letter.tier) {
+        const count = parseInt(message.content.replace(/[^0-9]/g, ""), 10)
+        if (isNaN(count)) {
+          message.reply(":thinking: Please enter a number")
+          return true
+        }
+        letter.tier = count >= TIER_THRESHOLD ? NICE_TIER : NAUGHTY_TIER
         recap(message, letter)
         return true
       }
@@ -55,10 +93,27 @@ module.exports = {
       if (message.content.match(/^confirm$/i)) {
         bot.db
           .run(
-            "INSERT INTO secretSanta (userId, letter, region) VALUES (?, ?, ?)",
-            [message.author.id, letter.content, letter.region],
+            "INSERT INTO secretSanta (userId, letter, address, region, tier) VALUES (?, ?, ?, ?, ?)" +
+              " ON CONFLICT(userId) DO UPDATE SET letter=?, address=?, region=?, tier=?",
+            [
+              message.author.id,
+              letter.content,
+              letter.address,
+              letter.region,
+              letter.tier,
+              letter.content,
+              letter.address,
+              letter.region,
+              letter.tier,
+            ],
           )
           .then(() => {
+            message.reply(
+              "All good! If you made a mistake or want to edit your letter, just repeat the whole process.",
+            )
+            bot.client.channels
+              .get(settings.channels.mod)
+              .send(`:santa: New letter from ${guildMember.displayName}`)
             bot.log(`Saved secret santa letter for ${message.author.username}`)
           })
           .catch(err =>
@@ -97,6 +152,7 @@ function recap(message, letter) {
       letter.content.replace(/^/gm, "> "),
       "Your full address is:",
       letter.address.replace(/^/gm, "> "),
+      `You will be matched with people from the **${letter.tier}** tier`,
       "If that is correct, please answer `CONFIRM`. If not, please type `CANCEL` and start over.",
     ].join("\n"),
   )
@@ -123,23 +179,41 @@ function santaList(bot, message) {
   const guild = bot.client.guilds.first()
 
   bot.db
-    .all(`SELECT userId, region FROM secretSanta ORDER BY region`)
-    .then(results =>
-      Promise.all(
-        results.map(r => guild.members.find(u => u.id === r.userId)),
-      ).then(volunteers => {
-        message.reply(
-          [
-            "",
-            ">>> ```",
-            volunteers
-              .map((v, i) => `${results[i].region} ${v.displayName}`)
-              .join("\n"),
-            "```",
-          ].join("\n"),
-        )
-      }),
+    .all(
+      `SELECT userId, region, tier FROM secretSanta ORDER BY region, tier DESC`,
     )
+    .then(async results => {
+      const guildMembers = new Map(
+        await Promise.all(
+          results.map(r => [
+            r.userId,
+            guild.members.find(u => u.id === r.userId),
+          ]),
+        ),
+      )
+
+      if (!results.length) {
+        message.reply(":shrug:")
+        return
+      }
+
+      const volunteerList = results
+        .map(
+          r =>
+            `[${r.tier}] ${r.region} ${guildMembers.get(r.userId).displayName}`,
+        )
+        .join("\n")
+
+      message.reply(
+        [
+          "\n>>> ```",
+          volunteerList,
+          "```",
+          `See full Secret Santa info at ${bot.settings.serverUrl +
+            WEB_ROUTE}?${bot.settings.adminPassword}`,
+        ].join("\n"),
+      )
+    })
 }
 
 function santaPair(bot, message) {
@@ -148,4 +222,31 @@ function santaPair(bot, message) {
   }
 
   message.reply("Not yet!")
+}
+
+function santaHandler(bot) {
+  return (req, res) => {
+    bot.log(`WEB ${WEB_ROUTE}`, req.query)
+    if (!(bot.settings.adminPassword in req.query)) {
+      return res.status(401).send("SKREEEOOOONK!!!")
+    }
+
+    const guild = bot.client.guilds.first()
+
+    bot.db
+      .all(`SELECT * FROM secretSanta ORDER BY region, tier DESC`)
+      .then(async results => {
+        const guildMembers = new Map(
+          await Promise.all(
+            results.map(r => [
+              r.userId,
+              guild.members.find(u => u.id === r.userId),
+            ]),
+          ),
+        )
+
+        res.render("santa", { results, guildMembers })
+      })
+      .catch(() => res.status(500).send("Internal server error"))
+  }
 }
