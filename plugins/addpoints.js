b3d9b996aadbabe7b78248cd233c5fa3dace9cf9
madx <madx+github@yapok.org>
2020-12-28 23:50:29 +0100

    addpoints: Send CNC rules when user reaches min required points

--- plugins/addpoints.js
+++ plugins/addpoints.js
@@ -14,6 +14,28 @@ const RANK_DOWN_GIFS = Array.from(
 
 const MIN_CNC_POINTS = 10
 
+const CNC_RULES = `
+Hello, you just unlocked access to the #contructive-critique channel on the minipainting server.
+
+Here you will find the rules for this channel. We've gotten many complaints about this channel and have been in talks about how to fix it for some time, it may change further in the future, but for now, here are the rules you must follow.
+
+> **1.** You must post a photo of the miniature in need of critique.
+> 
+> **2.** Your post must also contain the following information:
+> 
+> * What specific critique are you looking for? i.e. the overall paintjob, a specific part you've been working on, the hair, the sword, etc.
+> 
+> * What is the model for, so that we can get a rough idea of the standard you're aiming for, i.e. it's for an army, it's a D&D character or a centerpiece in an army.
+> 
+> **3.** If you are posting more than one photo, upload the photos to an album on www.imgur.com and post a link to the album. Posts that contain more than one photo, not in an album, will be removed without question. (We'll work on an integrated way to do this soon, but for now use imgur.)
+> 
+> **4.** If you are GIVING critique, your criticism MUST be constructive, do not simply tell someone "not enough contrast." You must also tell them HOW to change it, how to improve and **add** more contrast. If you do not, your post will be removed without notice.
+> 
+> **5.** This channel is for ***CRITIQUE***. It is NOT for general questions, it is not for early WIP photos, it is for critique of finished, or nearly finished work and nothing else. Do not post models here that have only been basecoated, or only have a few layers of paint on them. Only post finished or nearly finished works.
+
+For basic information and simple questions, #general-miniatures is the place to ask.
+`
+
 module.exports = {
   name: "addpoints",
   commands: [
@@ -163,6 +185,12 @@ async function setPoints(
     !member.roles.cache.has(bot.roles.cnc.id)
   ) {
     member.roles.add(bot.roles.cnc).catch(bot.logError)
+    try {
+      const dmChannel = await member.createDM()
+      await dmChannel.send(CNC_RULES)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   console.log(
