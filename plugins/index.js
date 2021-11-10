e543e610b0b98b8bc8a313e0977140f1dc624133
madx <madx+github@yapok.org>
2021-11-10 07:03:56 +0100

    BREAKING CHANGE: A clean slate

--- plugins/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs")
-const path = require("path")
-
-const plugins = fs
-  .readdirSync(__dirname)
-  .filter((f) => f !== "index.js")
-  .map((f) => require(path.join(__dirname, f)))
-
-module.exports = plugins
