bf9231c0eaa418016585305128b24fe4d1f33657
madx <madx+github@yapok.org>
2022-01-03 22:40:37 +0100

    fix(commands): Fix logging

--- workflows/commands.js
+++ workflows/commands.js
@@ -6,11 +6,13 @@ export default async function commands(bot) {
       try {
         await commands.get(interaction.commandName).execute(interaction)
         bot.logger.info(
-          `[commands] ${interaction.member.displayName} ${interaction.member} used ${interaction}`
+          "commands",
+          `${interaction.member.displayName} ${interaction.member} used ${interaction}`
         )
       } catch (error) {
         bot.logger.error(
-          `[commands] Error while executing ${error.commandName}`,
+          "commands",
+          `Error while executing ${error.commandName}`,
           error
         )
       }
