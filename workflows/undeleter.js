f509d1aa2f5b914774972a1600f298881e4da34a
madx <madx+github@yapok.org>
2024-01-25 10:48:05 +0100

    fix(workflows/undeleter): Ignore deletes for DM messages

--- workflows/undeleter.js
+++ workflows/undeleter.js
@@ -1,7 +1,13 @@
+import { ChannelType } from "discord.js"
+
 export default async function undeleter(bot) {
   bot.discord.on("messageDelete", onMessageDelete)
 
   async function onMessageDelete(message) {
+    if (message.channel.type === ChannelType.DM) {
+      return
+    }
+
     bot.logger.info(
       "undeleter",
       `Message by ${message.author} deleted: ${message}`,
