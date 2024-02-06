8006944a69c12f4a3c32d7cb1c71bbf862d96185
madx <madx+github@yapok.org>
2024-02-06 10:22:39 +0100

    fix(commands/deploy): Use defaultMemberPermission

--- commands/deploy.js
+++ commands/deploy.js
@@ -15,67 +15,12 @@ export default function deploy(bot) {
       await guild.commands.set(
         commandList.map((command) => ({
           ...pick(command, ["name", "description", "options"]),
-          defaultPermission: command.availability === bot.AVAILABILITY.PUBLIC,
+          defaultMemberPermission:
+            command.availability === bot.AVAILABILITY.PUBLIC,
         })),
       )
-
-      // TODO: Discord has discontinued setting command permissions for bots
-      // We'll be able to only specify if normal members can use them or not
-      // but we need to wait for a new discord.js (>13.7.0) version to support
-      // it
-      //
-      // Earlier:
-      // const guildCommands = await guild.commands.set(...)
-      //
-      // const fullPermissions = commandList
-      //   .map((command) => {
-      //     const permissions = resolvePermissions(bot, command.availability)
-      //     const guildCommand = guildCommands.find(
-      //       (c) => c.name === command.name
-      //     )
-      //
-      //     return {
-      //       id: guildCommand.id,
-      //       permissions,
-      //     }
-      //   })
-      //   .filter(({ permissions }) => Boolean(permissions))
-      //
-      // if (fullPermissions.length > 0) {
-      //   await guild.commands.permissions.set({ fullPermissions })
-      // }
 
       await interaction.reply("🤖 Commands deployed!")
     },
   }
 }
-
-// function resolvePermissions(bot, availability = bot.AVAILABILITY.PUBLIC) {
-//   const { id: atEveryoneId } = bot.guild.roles.cache.find(
-//     (role) => role.name === "@everyone"
-//   )
-//
-//   const resolved = [
-//     {
-//       id: bot.settings.botMasterId,
-//       type: "USER",
-//       permission: true,
-//     },
-//   ]
-//
-//   const roleIdMap = {
-//     [bot.AVAILABILITY.PUBLIC]: atEveryoneId,
-//     [bot.AVAILABILITY.MOD]: bot.roles.mod.id,
-//     [bot.AVAILABILITY.ADMIN]: bot.roles.admin.id,
-//   }
-//
-//   if (roleIdMap[availability]) {
-//     resolved.push({
-//       id: roleIdMap[availability],
-//       type: "ROLE",
-//       permission: true,
-//     })
-//   }
-//
-//   return resolved
-// }
